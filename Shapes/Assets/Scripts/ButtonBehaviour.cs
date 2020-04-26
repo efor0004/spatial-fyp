3519f3c65f1f55c4f19bc5594d4cf8953b131cfb
srsee3 <srsee3@users.noreply.github.com>
2020-04-26 19:12:15 +1000

    26/04/20
    Deleted unnecessary sprites and folders
    Set up menu structure

--- Shapes/Assets/Scripts/ButtonBehaviour.cs
+++ Shapes/Assets/Scripts/ButtonBehaviour.cs
@@ -1,58 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class ButtonBehaviour : MonoBehaviour
 {
-   // int n;
-    public void OnButtonPress()
-    {
-      //  Debug.Log("Entered On Button Press\n");
 
-      //  n++;
-      //  Debug.Log("Button clicked " + n + " times.");
-    }
+    private Vector3 velocity = Vector3.zero;
     public void LeftArrow() 
     {
-        if (TouchRotateSquarePurple.Active == true)
-        {
-            GameObject.Find("SquarePurple").transform.position += Global.menuOffset;
-            TouchRotateSquarePurple.shapeLoc = GameObject.Find("SquarePurple").transform.position;
-        }
+        //if (TouchRotateSquarePurple.Active == true)
+        //{
+        //    GameObject.Find("SquarePurple").transform.position += Global.shapeOffset;
+        //    TouchRotateSquarePurple.shapeLoc = GameObject.Find("SquarePurple").transform.position;
+        //}
 
-        if (TouchRotateTriangleRed.Active == true)
-        {
-            GameObject.Find("TriangleRed").transform.position += Global.menuOffset;
-            TouchRotateTriangleRed.shapeLoc = GameObject.Find("TriangleRed").transform.position;
-        }
+        //if (TouchRotateTriangleRed.Active == true)
+        //{
+        //    GameObject.Find("TriangleRed").transform.position += Global.shapeOffset;
+        //    TouchRotateTriangleRed.shapeLoc = GameObject.Find("TriangleRed").transform.position;
+        //}
 
-        if (TouchRotateDiamondYellow.Active == true)
-        {
-            GameObject.Find("DiamondYellow").transform.position += Global.menuOffset;
-            TouchRotateDiamondYellow.shapeLoc = GameObject.Find("DiamondYellow").transform.position;
-        }
+        //if (TouchRotateDiamondYellow.Active == true)
+        //{
+        //    GameObject.Find("DiamondYellow").transform.position += Global.shapeOffset;
+        //    TouchRotateDiamondYellow.shapeLoc = GameObject.Find("DiamondYellow").transform.position;
+        //}
 
     }
 
     public void RightArrow()
     {
-        if (TouchRotateSquarePurple.Active == true)
-        {
-            GameObject.Find("SquarePurple").transform.position -= Global.menuOffset;
-            TouchRotateSquarePurple.shapeLoc = GameObject.Find("SquarePurple").transform.position;
-        }
+        //if (TouchRotateSquarePurple.Active == true)
+        //{
+        //    GameObject.Find("SquarePurple").transform.position -= Global.shapeOffset;
+        //    TouchRotateSquarePurple.shapeLoc = GameObject.Find("SquarePurple").transform.position;
+        //}
 
-        if (TouchRotateTriangleRed.Active == true)
-        {
-            GameObject.Find("TriangleRed").transform.position -= Global.menuOffset;
-            TouchRotateTriangleRed.shapeLoc = GameObject.Find("TriangleRed").transform.position;
-        }
+        //if (TouchRotateTriangleRed.Active == true)
+        //{
+        //    GameObject.Find("TriangleRed").transform.position -= Global.shapeOffset;
+        //    TouchRotateTriangleRed.shapeLoc = GameObject.Find("TriangleRed").transform.position;
+        //}
 
-        if (TouchRotateDiamondYellow.Active == true)
-        {
-            GameObject.Find("DiamondYellow").transform.position -= Global.menuOffset;
-            TouchRotateDiamondYellow.shapeLoc = GameObject.Find("DiamondYellow").transform.position;
-        }
+        //if (TouchRotateDiamondYellow.Active == true)
+        //{
+        //    GameObject.Find("DiamondYellow").transform.position -= Global.shapeOffset;
+        //    TouchRotateDiamondYellow.shapeLoc = GameObject.Find("DiamondYellow").transform.position;
+        //}
     }
 
+    public void StartButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.leftPosition;
+        GameObject.Find("ModeMenu").transform.localPosition = Global.centrePosition;
+
+        // GameObject.Find("MainMenu").transform.localPosition = Vector3.SmoothDamp(Global.centrePosition, Global.leftPosition, ref velocity, 2f);   
+        // GameObject.Find("ModeMenu").transform.localPosition = Vector3.SmoothDamp(Global.rightPosition, Global.centrePosition, ref velocity, 2f);
+    }
+
+    public void SettingsButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.rightPosition;
+        GameObject.Find("SettingsMenu").transform.localPosition = Global.centrePosition;
+    }
+
+    public void ParentalInfoButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.topPosition;
+        GameObject.Find("ParentalinfoMenu").transform.localPosition = Global.centrePosition;
+    }
+
+    public void ModeBackButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.centrePosition;
+        GameObject.Find("ModeMenu").transform.localPosition = Global.leftPosition;
+    }
+
+    public void SettingsBackButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.centrePosition;
+        GameObject.Find("SettingsMenu").transform.localPosition = Global.rightPosition;
+    }
+
+    public void ParentalinfoBackButton()
+    {
+        GameObject.Find("MainMenu").transform.localPosition = Global.centrePosition;
+        GameObject.Find("ParentalinfoMenu").transform.localPosition = Global.topPosition;
+    }
+
+    public void GreenModeButton()
+    {
+        Global.greenMode = true;
+        Global.redMode = false;
+
+        SceneManager.LoadScene("Lands"); 
+
+    }
+
+    public void RedModeButton()
+    {
+        Global.redMode = true;
+        Global.greenMode = false;
+
+        SceneManager.LoadScene("Lands");
+    }
+
+    public void ModeInfoButton()
+    {
+        GameObject.Find("InfoPopup").transform.localPosition = Global.centrePosition;
+    }
+
+    public void InfoCloseButton()
+    {
+        Debug.Log("Entered closs button"); 
+        GameObject.Find("InfoPopup").transform.localPosition = Global.rightPosition;
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
--- Shapes/Assets/Scripts/Global.cs
+++ Shapes/Assets/Scripts/Global.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Global : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public static Vector3 menuOffset = new Vector3 (3,0,0);
+    public static Vector3 shapeOffset = new Vector3(3, 0, 0);
+
+    public static Vector3 currentVal1 = new Vector3(0, 0, 0);
+    public static Vector3 currentVal2 = new Vector3(0, 0, 0);
+
+
+    public static Vector3 leftPosition = new Vector3(-2000, -60, 0);
+    public static Vector3 rightPosition = new Vector3(2000, -60, 0);
+    public static Vector3 bottomPosition = new Vector3(0, -2000, 0);
+    public static Vector3 topPosition = new Vector3(0, 2000, 0);
+    public static Vector3 centrePosition = new Vector3(0, -60, 0);
+
+
     public static float positionTolerance = 0.35f;  // x y 
     public static float rotationTolerance = 0.15f;  //z
+    public static bool greenMode = false;
+    public static bool redMode = false;
 
 
     void Start()
@@ -19,6 +34,7 @@ public class Global : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
 }
