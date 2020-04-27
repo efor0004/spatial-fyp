ca7de334b5a65d93070b47af332a05a6753329c9
Eleanor Forwood <efor0004@student.monash.edu>
2020-04-27 14:15:07 +1000

    QUILT: Randomise options order

--- quilt/app/scripts/Question.gd
+++ quilt/app/scripts/Question.gd
@@ -5,9 +5,11 @@ onready var current_question = 1
 onready var questions_per_level = 10
 onready var max_levels = 1
 
+func _ready():
+	set_textures()
+
 func _on_Next_Button_pressed():
 	print("button pressed")
-	var children = get_children()
 	
 	current_question += 1
 	
@@ -19,6 +21,10 @@ func _on_Next_Button_pressed():
 		current_level = 1
 		current_question = 1
 	
+	set_textures()
+
+func set_textures():
+	var children = get_children()
 	var base_path = "res://assets/sprites/questions/level_%d/question_%d" % [current_level, current_question]
 	
 	print("Level %d, Question %d" % [current_level, current_question])
@@ -33,10 +39,26 @@ func set_holey_quilt_texture(base_path, children):
 	holey_quilt.set_texture(holey_quilt_texture)
 
 func set_options_textures(base_path, children):
-	for i in range(1,4):
+	var indices = range(1,4)
+	var random_indices = shuffleList(indices)
+	print(indices)
+	print(random_indices)
+	for i in indices:
 		var option = children[i]
-		var option_path_suffix = "/option_%d.png" % i
+		var shape_no = random_indices[i - 1]
+		var option_path_suffix = "/option_%d.png" % shape_no
 		var option_path = base_path + option_path_suffix
 		var option_texture = load(option_path)
 		option.set_texture(option_texture)
 		
+func shuffleList(list):
+	var list_copy = list.duplicate()
+	var shuffled_list = []
+	for i in range(list_copy.size()):
+		var index_list = range(list_copy.size())
+		randomize()
+		var x = randi()%index_list.size()
+		shuffled_list.append(list_copy[x])
+		index_list.remove(x)
+		list_copy.remove(x)
+	return shuffled_list
