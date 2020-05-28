b17e0f9efb94de64fac565b058d97ab1cda82b91
forwoodthinker <efor0004@student.monash.edu>
2020-05-28 20:13:41 +1000

    QUILT: Disable other options during animations

--- quilt/app/scripts/Option.gd
+++ quilt/app/scripts/Option.gd
@@ -6,6 +6,7 @@ var option_file_text = "option_"
 
 onready var animation_player = get_parent().get_parent()
 onready var question = animation_player.get_parent()
+onready var all_animation_players = get_all_animation_players()
 
 func pressed():
 	var is_correct = is_correct_answer()
@@ -20,7 +21,7 @@ func pressed():
 		print('incorrect')
 
 func is_correct_answer():
-	var option_sprite = self.get_child(0)
+	var option_sprite = self.get_node("Light2D")
 	var option_shape = option_sprite.get_texture()
 	var option_shape_path = option_shape.get_load_path()
 	
@@ -31,7 +32,29 @@ func is_correct_answer():
 	return opt_no == 1
 
 func _input_event(_viewport, event, _shape_idx):
+	if is_animation_happening():
+		return
+	
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
 		pressed()
+
+func is_animation_happening():
+	var is_animating = false
+	
+	for anim in all_animation_players:
+		if anim.is_playing():
+			is_animating = true
+			break
+	
+	return is_animating
+
+func get_all_animation_players():
+	var all_animation_players = []
+	
+	for node in question.get_children():
+		if node is AnimationPlayerWait:
+			all_animation_players.append(node)
+	
+	return all_animation_players
