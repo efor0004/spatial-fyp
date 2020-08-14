6d8d9b8f14a9e201c87afe8f0b9f2882e18b309e
srsee3 <srsee3@users.noreply.github.com>
2020-08-14 21:54:41 +1000

    14/08/20 part 3
    Created unique translation/rotation handler for MovieMaker. Added start/stop button and countdown timer (no functionality yet) to MovieMaker.

--- Shapes/Assets/Scripts/MovieMaker.cs
+++ Shapes/Assets/Scripts/MovieMaker.cs
@@ -8,71 +8,17 @@ public class MovieMaker : MonoBehaviour
 {
     //the script that controls all of the puzzles in the Mouse Shapes scene
 
-    delegate void PuzzleMethod();                                       //creates an empty method
-    List<PuzzleMethod> Puzzle = new List<PuzzleMethod>();               //creates a list of empty methods
-
-    // int PuzzlesPerLevel = 5;
-   // Image Mask;
-    //Text LevelText;
   
     int TotalPuzzles = 1;
     int n = 0;
 
-    //void CreateList()
-    //{
-    //    //populates the list of empty methods with a function call for each puzzle
-    //    //adapted from https://answers.unity.com/questions/873650/how-to-make-a-list-or-array-of-functions-with-type.html 
-
-    //    Puzzle.Add(Puzzle1);
-
-    //}
-
-
     void Start()
     {
-        // Start is called before the first frame update
-
-        Global.NextPuzzleReady = true;                                     //set as true every time the scene is opened        
-       // Mask = GameObject.Find("Mask").GetComponent<Image>();
-       // LevelText = GameObject.Find("LevelText").GetComponent<Text>();
-
-        //CreateList();                                                    //initiate this list with function calls for all avilable puzzles in this world
         Puzzle1();
     }
     void Update()
     {
-        ////called each frame
-        //if (Global.piecesPlaced == Global.puzzlePieces)                    //puzzle completion 
-        //{
-        //    if (n + 1 == TotalPuzzles)
-        //    {                                                            //if this is the last puzzle in the world
-        //        Global.WorldComplete();
-        //    }
 
-        //    else if (Global.MousePuzzle == 5)
-        //    {                                                            //if this is the last puzzle in the level
-        //        Global.LevelComplete();
-        //    }
-
-        //    else
-        //    {
-        //        Global.PuzzleComplete();
-        //    }
-        //}
-
-        //if (Global.NextPuzzleReady == true)
-        //{
-        //    if (n + 1 == TotalPuzzles) //if there are no more games
-        //    {
-        //        Global.MouseComplete = 1;
-        //        SceneManager.LoadScene("Worlds"); //go to main menu
-        //    }
-        //    else
-        //    {
-        //        n = (Global.MouseLevel - 1) * 5 + Global.MousePuzzle;              //index of array = puzzle number -1
-        //        Puzzle[n]();                                                       //calls the puzzle by indexing the array of function calls
-        //    }
-        //}
 
     }
 
@@ -84,15 +30,9 @@ public class MovieMaker : MonoBehaviour
 
         Debug.Log("Puzzle 1 run"); //////
 
-        GameObject.Find("PreviousButton").GetComponent<Button>().interactable = false; //make previous button false on first puzzle
+       // GameObject.Find("PreviousButton").GetComponent<Button>().interactable = false; //make previous button false on first puzzle
 
         Global.NextPuzzleReady = false; //check this is the case of not completing a puzzle or returning to progress!!!!!!!
-
-        //record level and puzzle numbers
-        Global.MouseLevel = 1;
-        Global.MousePuzzle = 1;
-
-       // Global.ProgressBar(Global.PuzzlesPerLevel, Global.MousePuzzle - 1, Mask, LevelText, Global.MouseLevel); //updates the progress bar
 
         //set number of pieces in the puzzle (those to be placed)
         Global.puzzlePieces = 1;
@@ -101,9 +41,8 @@ public class MovieMaker : MonoBehaviour
         Global.RenderShapeFixed("Shape0", "TriangleI", new Vector3(0.1424218f, 0.4473302f, 0f), new Vector3(0f, 0f, 0f), new Vector3(1f, 1f, 1f), "Shape1", new Vector4(1f, 0f, 0.05237484f, 1f), false, 0, false);
         Global.RenderShapeVariable("Shape1", "Pentagon", new Vector3(0.04f, 3.84f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0.531f, 0.531f, 0.531f), "Shape2", new Vector4(1f, 0.7064719f, 0f, 1f), false, 1, false);
 
-
         //spawn target image
-         Global.RenderPuzzleImage("MS1");  //has the touchrotate script attached --> run after all shapes are loaded
+        // Global.RenderPuzzleImage("MS1");  //has the touchrotate script attached --> run after all shapes are loaded
 
     }
 
---
+++ Shapes/Assets/Scripts/MovieRotate.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Linq;
+
+public class MovieRotate : MonoBehaviour
+{
+    // controls translation and rotation for MovieMaker
+
+    private Collider2D myCollider;
+
+    Vector3 wp;
+    Vector2 touchPos;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Global.LeftArrowActive == true)   //controls left and right arrows (verify how two commands work)
+        {
+            GameObject.Find("LeftArrow").GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            GameObject.Find("LeftArrow").GetComponent<Button>().interactable = false;
+        }
+
+        if (Global.RightArrowActive == true)
+        {
+            GameObject.Find("RightArrow").GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            GameObject.Find("RightArrow").GetComponent<Button>().interactable = false;
+        }
+
+
+
+        if (Input.touchCount > 0)                                                                                                         //at least one touch detected                                                                    
+        {
+            wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);                                                              //Vector3 of the touch location on the screen
+            touchPos = new Vector2(wp.x, wp.y);
+
+            //for each name in namearray//
+
+            GameObject go = GameObject.Find("Shape1");
+
+            if (go)
+            {
+                myCollider = go.gameObject.GetComponent<CircleCollider2D>();
+
+                if (myCollider)                                                                                                     //collider will not exist for the anchor shape0
+                {
+                    if (myCollider == Physics2D.OverlapPoint(touchPos))                                                             //if the touch position overlaps with the 2D collider of the shape
+                    {
+                        if (Input.touchCount == 2)
+                        {                                                                                                            //updates rotation                                                                                                     
+                            Quaternion desiredRotation = go.gameObject.transform.rotation;                                           //start desiredRotation as the current orientation of the shape
+                            DetectTouchMovement.Calculate();                                                                         //determines turnAngle and turnAngleDelta from 2 finger rotation on screen
+
+                            if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0)                                                  //if the detected turn angle is large enough and the shape is not small/circular
+                            {
+                                Vector3 rotationDeg = Vector3.zero;
+                                rotationDeg.z = DetectTouchMovement.turnAngleDelta;
+                                desiredRotation *= Quaternion.Euler(rotationDeg);                                                    //update the desiredRotation to include this change in angle
+
+                                go.gameObject.transform.rotation = desiredRotation;                                                  //upate the shape rotated orientation
+                            }
+                        }
+                        if (Input.touchCount == 1)
+                        {                                                                                                             //updates shape translated position https://answers.unity.com/questions/991083/dragging-a-2d-sprite-with-touch.html 
+                            go.gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                                                       Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+                                                                        0.0f);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
