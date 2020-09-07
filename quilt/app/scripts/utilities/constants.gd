8ea3c8210a52595284e33fe52d663a11630c9396
forwoodthinker <efor0004@student.monash.edu>
2020-09-07 17:51:02 +1000

    QUILT: Add level 5 + fix progress quilt scale

--- quilt/app/scripts/utilities/constants.gd
+++ quilt/app/scripts/utilities/constants.gd
@@ -126,16 +126,80 @@ var level_3_option_rotations = [
 	-135,
 	135,
 	-135,
-	135
+	135,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0
 ]
 
 var level_4_option_rotations = 0
+
+var level_5_option_rotations = [
+	-90,
+	90,
+	180,
+	90,
+	180,
+	-90,
+	180,
+	90,
+	-90,
+	-90,
+	180,
+	0,
+	90,
+	-90,
+	180,
+	90,
+	90,
+	-90,
+	180,
+	180,
+	90,
+	-90,
+	-90,
+	180,
+	90,
+	0,
+	-90,
+	180,
+	90,
+	180,
+	90,
+	-90,
+	180,
+	-90,
+	90,
+	-90,
+	180,
+	90,
+	90,
+	-90,
+	-90,
+	90,
+	-90,
+	90,
+	90,
+	-90
+]
 
 export(Array) var level_option_rotations = [
 	{ "normal": level_1_option_rotations, "hard": level_1_hard_option_rotations }, # level 1
 	{ "normal": level_2_option_rotations, "hard": level_2_hard_option_rotations }, # level 2
 	{ "normal": level_3_option_rotations }, # level 3
-	{ "normal": level_4_option_rotations } # level 4
+	{ "normal": level_4_option_rotations }, # level 4
+	{ "normal": level_5_option_rotations } # level 5
 ]
 
 var hard_questions_per_level = 4
@@ -144,7 +208,8 @@ export(Array) var num_hard_questions_per_level = [
 	{ "required": hard_questions_per_level, "available": 13 }, # level 1
 	{ "required": hard_questions_per_level, "available": 30 }, # level 2
 	{ "required": 0, "available": 0 }, # level 3
-	{ "required": 0, "available": 0 } # level 4
+	{ "required": 0, "available": 0 }, # level 4
+	{ "required": 0, "available": 0 } # level 5
 ]
 
 export var questions_per_level = 10
@@ -152,11 +217,12 @@ export var questions_per_level = 10
 export(Array) var questions_available_per_level = [
 	30, # level 1
 	42, # level 2
-	25, # level 3
-	53
+	39, # level 3
+	39, # level 4
+	46 # level 5
 ]
 
-export var max_levels = 4
+export var max_levels = 5
 
 export var max_questions_per_file = 10
 
