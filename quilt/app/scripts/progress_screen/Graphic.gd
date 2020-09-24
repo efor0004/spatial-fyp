aff6a5ed08c662c6b4c0be2bba48060c8eadb59f
forwoodthinker <efor0004@student.monash.edu>
2020-09-24 16:25:15 +1000

    QUILT: Character + quilt on progress screen

--- quilt/app/scripts/progress_screen/Graphic.gd
+++ quilt/app/scripts/progress_screen/Graphic.gd
@@ -1,14 +1,16 @@
-extends Sprite
+extends Node2D
 
 const Constants = preload("../utilities/constants.gd")
 onready var constants = Constants.new()
 
+onready var quilt = get_node("Quilt")
+
 func _ready():
 	var current_level = global.current_level - 1
-	var path = "res://assets/screens/progress/level_%d.png" % current_level
+	var path = "res://assets/screens/progress/quilt/level_%d.png" % current_level
 	
 	if (global.current_level >= constants.max_levels && global.current_question >= constants.questions_per_level):
-		path = "res://assets/screens/progress/all_levels.png"
+		path = "res://assets/screens/progress/quilt/level_%d.png" % constants.max_levels
 	
-	var graphic_texture = load(path)
-	self.texture = graphic_texture
+	var quilt_texture = load(path)
+	quilt.texture = quilt_texture
